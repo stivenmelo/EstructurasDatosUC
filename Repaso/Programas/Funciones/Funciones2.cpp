4063f9912e4991d08b6b7f8a98569d6b587d4e36
stiven melo <51790572+stivenmelo@users.noreply.github.com>
2023-08-10 19:37:01 -0500

    Update Funciones2.cpp
    se agregan funciones con retorno

--- Repaso/Programas/Funciones/Funciones2.cpp
+++ Repaso/Programas/Funciones/Funciones2.cpp
@@ -1,6 +1,6 @@
 #include <iostream>       // Prueba de parámetros para función main
 using namespace std;
-
+//funciones Void(sin retorno)
 void LeerVector(int arreglo[10]){
 	for(int i=0;i<10;i++){
 		cout<<"Digite la posicion "<<i+1<<" del vector:";
@@ -8,16 +8,33 @@ void LeerVector(int arreglo[10]){
 	}
 };
 
-void MostrarVector(int arreglo[10]){
+void MostrarVector(int arreglo[10], int suma){
 	for(int i=0;i<10;i++){
 		cout<<"\nEn la posicion "<<i+1<<" el valor es: "<<arreglo[i];
-	
 	}
+	
+	cout<<"\nla sumatoria es: "<<suma;
 };
+//funciones con retorno
+int Sumatoria(int arreglo[10]){
+	int total = 0;
+	
+	for(int i =0 ; i<10; i++){
+		total=total + arreglo[i];
+	}
+	
+	return total;
+} 
 
 int main() {
+	int suma = 0;
+	
+	//funciones Con array funcionan por referencia, es decir el mismo espacio en memoria
 	 int v[10];
 	 LeerVector(v);
-	 MostrarVector(v);
+	 
+	 
+	 suma = Sumatoria(v);
+	 MostrarVector(v, suma);
 }
 
