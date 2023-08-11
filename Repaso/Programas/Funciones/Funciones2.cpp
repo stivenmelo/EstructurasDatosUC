1507e36d0ca39392f16b1d7a1c21f8e7cf6cceac
stiven melo <51790572+stivenmelo@users.noreply.github.com>
2023-08-10 19:51:04 -0500

    Update Funciones2.cpp
    se agrega media, minimo y maximo

--- Repaso/Programas/Funciones/Funciones2.cpp
+++ Repaso/Programas/Funciones/Funciones2.cpp
@@ -8,12 +8,16 @@ void LeerVector(int arreglo[10]){
 	}
 };
 
-void MostrarVector(int arreglo[10], int suma){
+void MostrarVector(int arreglo[10], int suma, float media, int min, int max){
 	for(int i=0;i<10;i++){
 		cout<<"\nEn la posicion "<<i+1<<" el valor es: "<<arreglo[i];
 	}
 	
-	cout<<"\nla sumatoria es: "<<suma;
+	cout<<"\nLa sumatoria es: "<<suma;
+	cout<<"\nLa media es: "<<media;
+	cout<<"\nEL valor minimo en el vector es : "<<min;
+	cout<<"\nEL valor maximo en el vector es : "<<max;
+
 };
 //funciones con retorno
 int Sumatoria(int arreglo[10]){
@@ -26,8 +30,37 @@ int Sumatoria(int arreglo[10]){
 	return total;
 } 
 
+float Media(int arreglo[10]){
+	return (float) Sumatoria(arreglo)/10;
+}
+
+int Min(int arreglo[10]){
+	int min =arreglo[0];
+	 
+	 for(int i =0 ;i<10 ;i++){
+	 	if(min>arreglo[i]){
+	 		min = arreglo[i];
+		 }
+	 }
+	 
+	 return min;
+}
+
+int Max(int arreglo[10]){
+	int max =arreglo[0];
+	 
+	 for(int i =0 ;i<10 ;i++){
+	 	if(max<arreglo[i]){
+	 		max = arreglo[i];
+		 }
+	 }
+	 
+	 return max;
+}
+
 int main() {
-	int suma = 0;
+	int suma = 0,min =0, max =0;
+	float media = 0;
 	
 	//funciones Con array funcionan por referencia, es decir el mismo espacio en memoria
 	 int v[10];
@@ -35,6 +68,11 @@ int main() {
 	 
 	 
 	 suma = Sumatoria(v);
-	 MostrarVector(v, suma);
+	 media = Media(v);
+	 min = Min(v);
+	 max = Max(v);
+	 
+	 MostrarVector(v, suma,media,min,max);
 }
+
 
