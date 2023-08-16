8763b15af0de80fd656db2f8c70b31a933229c49
stiven melo <51790572+stivenmelo@users.noreply.github.com>
2023-08-16 18:50:36 -0500

    Update ejecicioBuss.cpp
    se agregan metodos de media y suma de pasajeros

--- Repaso/Programas/Funciones/ejecicioBuss.cpp
+++ Repaso/Programas/Funciones/ejecicioBuss.cpp
@@ -8,13 +8,18 @@ void leer(int bus[10]){
 	}
 }
 
-void imprimir(int bus[10]){
+void imprimir(int bus[10], int sobrecupo, int totalPasajeros,float mediaPasajeros){
 	for(int i = 0 ; i<10 ; i++){
 		cout<<endl<<"la cantidad de pasajeros del bus"<<i+1<<"es:"<<bus[i];
 	}
+	
+	cout<<endl<<"buses con Sobrecupo: "<<sobrecupo;
+	cout<<endl<<"Total pasajeros: "<<totalPasajeros;
+	cout<<endl<<"Media de pasajeros: "<<mediaPasajeros;
 }
 
-int calcularSobrecupo(int bus[10], int sobrecupo){
+int calcularSobrecupo(int bus[10]){
+	int sobrecupo =0;
 	for(int i = 0 ; i<10 ; i++){
 		if(bus[i]> 150){
 			sobrecupo++;
@@ -25,13 +30,31 @@ int calcularSobrecupo(int bus[10], int sobrecupo){
 	//cout<<endl<<"sobrecupo en funcion: "<<sobrecupo;
 }
 
+int SumatoriaPasajeros(int buses[10]){
+	int suma =0 ;
+	for(int i = 0 ; i<10 ; i++){
+		suma += buses[i];
+	}
+	return suma;
+}
+
+
+float MediaPasajeros(int totalPasajeros){
+	return totalPasajeros/10;
+}
+
+
+
 int main () {
-  int buses[10], sobrecupo =0;
+  int buses[10], sobrecupo =0,totalPasajeros =0 ;
+  float mediaPasajeros;
   leer(buses);
-  sobrecupo = calcularSobrecupo(buses,sobrecupo);
-  imprimir(buses);
+  sobrecupo = calcularSobrecupo(buses);
+  totalPasajeros =SumatoriaPasajeros(buses);
   
-  cout<<endl<<"Sobrecupo: "<<sobrecupo;
+  mediaPasajeros = MediaPasajeros(totalPasajeros);
+  imprimir(buses,sobrecupo,totalPasajeros,mediaPasajeros);
   
+  	//cout<<endl<<"sobrecupo en main: "<<sobrecupo;
 }
 
